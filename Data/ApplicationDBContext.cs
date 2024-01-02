a41cd13fb816c51a783addfd3acfc43c0c402a5c
paulsontiti@gmail.com <paulsontiti@gmail.com>
2024-01-02 12:42:48 +0100

    Added InitialSetup Migration - Seed Data for Book Category

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,25 @@
+﻿using BookLibrary_Razor.Model;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary_Razor.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>().HasData(
+                new BookCategory { Id = 1, Name = "Action", DisplayOrder = 1 },
+                 new BookCategory { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                  new BookCategory { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ Models/BookCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookLibrary_Razor.Model
+{
+    public class BookCategory
+    {
+
+        public required int Id { get; set; }
+        [DisplayName("Category Name")]
+        [Required]
+        [MaxLength(30)]
+        public required String Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(0, 100)]
+        public required int DisplayOrder { get; set; }
+    }
+}
